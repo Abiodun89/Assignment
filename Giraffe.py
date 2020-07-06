7dfa9dd7d3311005179f585c5388a6d95adf72d5
Abiodun Aina <61989475+Abiodun89@users.noreply.github.com>
2020-07-06 10:17:47 +0300

    Add files via upload

--- Giraffe.py
+++ Giraffe.py
@@ -1,11 +1,5 @@
-#program to reverse word in a string
-string = "programming study to love I"
-def reverseString(s):
-    wordArray = s.split(' ')
-    output = []
-    for word in wordArray:
-     output.insert(0,word)
-    return " ".join(output)
-print(reverseString(string))
+inputstr = "geeks quiz practice code"
+print (inputstr[-1::-1])
+
 
 
